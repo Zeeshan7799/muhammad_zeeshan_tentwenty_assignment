f25a87840434f3dc5c7ae711da92d93e346cea80
Muhammad Zeeshan <zeeshan.7799@gmail.com>
2023-06-12 00:44:20 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
       builder: (context, child){
         return DismissKeyboard(
           child: MaterialApp(
+            debugShowCheckedModeBanner: false,
             title: 'Flutter Demo',
             theme: ThemeData(
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
